76268e98d2570aa6becd663a6bb533cd8a4cb0eb
samuraipapa <yury.gitman@gmail.com>
2014-02-22 11:11:53 -0500

    Giving Categories and Category names to SKSpriteNodes

--- NSFastEnumeration Example  For Loop/MyScene.m
+++ NSFastEnumeration Example  For Loop/MyScene.m
@@ -8,6 +8,17 @@
 
 #import "MyScene.h"
 
+static NSString* smallSquareCategoryName = @"smallSquare";
+static NSString* bigSquareCategoryName = @"bigSquare";
+//static NSString* blockCategoryName = @"block";
+//static NSString* blockNodeCategoryName = @"blockNode";
+
+static const uint32_t smallSquareCategory  = 0x1 << 0;  // 00000000000000000000000000000001
+static const uint32_t bigSquareCategory = 0x1 << 1; // 00000000000000000000000000000010
+//static const uint32_t blockCategory = 0x1 << 2;  // 00000000000000000000000000000100
+//static const uint32_t paddleCategory = 0x1 << 3; // 00000000000000000000000000001000
+
+
 @implementation MyScene{
 
     SKSpriteNode *squareBig1;
@@ -24,6 +35,19 @@
     self.backgroundColor = [SKColor blackColor];
     self.scaleMode = SKSceneScaleModeAspectFit;
     self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    [self.physicsWorld setGravity:CGVectorMake(0, -2)];
+    
+    
+    
+//    // 1 Create a physics body that borders the screen
+//    SKPhysicsBody* borderBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+//    // 2 Set physicsBody of scene to borderBody
+//    self.physicsBody = borderBody;
+    // 3 Set the friction of that physicsBody to 0
+//    self.physicsBody.friction = 0.0f;
+    
+    
+    
 }
 
 -(id)initWithSize:(CGSize)size {    
@@ -88,14 +112,22 @@
     squareBig1 = [SKSpriteNode spriteNodeWithColor:[SKColor yellowColor] size:CGSizeMake(50, 50)];
     [squareBig1 setPosition:location];
     squareBig1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:squareBig1.size];
+    squareBig1.name = bigSquareCategoryName;
+    squareBig1.physicsBody.categoryBitMask = bigSquareCategory;
+    
     [self addChild:squareBig1];
+    
+    
 }
 
 -(void) drawBigSquare2InLocation: (CGPoint) location{
     squareBig2 = [SKSpriteNode spriteNodeWithColor:[SKColor redColor] size:CGSizeMake(50, 50)];
     [squareBig2 setPosition:location];
     squareBig2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:squareBig2.size];
-
+    squareBig1.name = bigSquareCategoryName;
+    squareBig1.physicsBody.categoryBitMask = bigSquareCategory;
+    
+    
     [self addChild:squareBig2];
     
     myJointLimit = [SKPhysicsJointLimit jointWithBodyA:squareBig1.physicsBody bodyB:squareBig2.physicsBody anchorA:squareBig1.position anchorB:squareBig2.position  ];
@@ -108,6 +140,12 @@
     smallSquare = [SKSpriteNode spriteNodeWithColor:[SKColor yellowColor] size:CGSizeMake(5, 5)];
     [smallSquare setPosition:location];
     [smallSquare setColor:[SKColor orangeColor]];
+    
+    smallSquare.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:smallSquare.size];
+    smallSquare.name = smallSquareCategoryName;
+    smallSquare.physicsBody.categoryBitMask = smallSquareCategory;
+
+    
     [self addChild:smallSquare];
 }
 
